07b2d2557ae54a0f91cba43140c89410df524f50
FabioB8a <fabiobuitra@gmail.com>
2022-11-08 22:32:52 -0500

    Tercer commit, cambio de letras

--- src/main/java/github/demo/HelloApplication.java
+++ src/main/java/github/demo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Cambio de datos en todo!");
+        stage.setTitle("Cambio de datos en todaado!");
         stage.setScene(scene);
         stage.show();
     }
